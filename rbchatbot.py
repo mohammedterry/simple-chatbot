9b70a6a87db12f60f695c3ed3254c68cc5eee675
Mohammed b. Terry b. Jack <32687826+mohammedterry@users.noreply.github.com>
2018-01-21 13:34:11 +0000

    Update rbchatbot.py

--- rbchatbot.py
+++ rbchatbot.py
@@ -1,24 +1,19 @@
-intentions = { "hello": "hello",
-   "hi": "hello",
-   "hiya": "hello",
-   "howdy": "hello",
-   "hey": "hello",
-   "heya":"hello",
-   "goodbye":"goodbye",
-   "bye": "goodbye",
-   "ciao":"goodbye",
-   "see you":"goodbye",
-   "go away":"goodbye"}
-responses = {"hello": "how are you?",
-    "help":"how exactly can i help?",
-    "goodbye":"take care"}
+import random
+
+intents = "hello/goodbye".split("/")
+hello = "hello/hi/heya/hiya".split("/")
+goodbye = "goodbye/bye/ciao/see you/go away".split("/")
+confused = "what do you mean?/???/i dont get it?/huh?".split("/")
+responses = {"hello": "hello",
+    "goodbye":"goodbye"}
+intentions = {phrase:intent for intent in intents for phrase in eval(intent)}
 
 reply = ""
-while reply != "take care":
-phrase = input("> ")
-if phrase in intentions:
-reply = responses[intentions[phrase]]
-print(reply)
-else:
-print("sorry i dont know what you mean?")
-
+while reply != "goodbye":
+	phrase = input("> ")
+	if phrase in intentions:
+		reply = responses[intentions[phrase]]
+	else:
+		reply = "confused"
+	nl_reply = random.choice(eval(reply))
+	print(nl_reply)
