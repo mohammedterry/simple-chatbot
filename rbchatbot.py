3fbdea9efe36e2679d4ecc46dafa294986d6b5f5
Mohammed b. Terry b. Jack <32687826+mohammedterry@users.noreply.github.com>
2018-01-21 19:21:38 +0000

    Update rbchatbot.py

--- rbchatbot.py
+++ rbchatbot.py
@@ -1,19 +1,26 @@
 import random
 
-intents = "hello/goodbye/confused".split("/")
-hello = "hello/hi/heya/hiya".split("/")
-goodbye = "goodbye/bye/ciao/see you/go away".split("/")
-confused = "what do you mean?/???/i dont get it?/huh?".split("/")
-responses = {"hello": "hello",
-    "goodbye":"goodbye"}
-intentions = {phrase:intent for intent in intents for phrase in eval(intent)}
+intents = {"hi": "hello/hi/heya/hiya/howdy/heyho/whats up?/how are you?".split("/"),
+    "bye":"goodbye/bye/bye bye/ciao/see you/laters/adios".split("/"),
+    "confused":"??/what do you mean?/i dont get it?/huh?/what?/sorry?/excuse me?/i beg your pardon?/come again?/i dont understand".split("/"),
+    "rude":"go away/i hate you/you are so annoying/be quiet".split("/"),
+    "angry":"grrr/hey! thats not nice/you're so rude/someone's touchy!/goodness me!/what's wrong with you!".split("/")}
 
-reply = ""
-while reply != "goodbye":
-	phrase = input("> ")
-	if phrase in intentions:
-		reply = responses[intentions[phrase]]
-	else:
-		reply = "confused"
-	nl_reply = random.choice(eval(reply))
-	print(nl_reply)
+responses = {"hi":"hi",
+    "bye":"bye",
+    "rude":"angry",
+    "angry":"rude",
+    "unknown phrase":"confused"}
+
+known_phrases = {phrase:intent for intent in intents.keys() for phrase in intents[intent]}
+
+intent = "hi there"
+print(intent)
+while intent != "bye":
+    phrase = input("> ")
+    if phrase in known_phrases:
+        intent = known_phrases[phrase]
+    else:
+        intent = "unknown phrase"
+    reply = random.choice(intents[responses[intent]])
+    print(reply)
